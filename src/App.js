39497800ca67e0f5a290189aa3afbb93f6ddd6b9
Eduard5555 <gabrielyan101101@gmail.com>
2022-11-26 18:21:20 +0400

    test

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import './App.css';
 import Footer from './Component/footer';
 import Header from './Component/Header';
 
-
-
-
 function App() {
   return (
     <div className="App">
       <Header />
+      {/* Hellow */}
       <Footer/>
     </div>
   );
